c16ab70f03686017c2e44abb425af41995838f0e
steve <panj3457@myumanitoba.ca>
2021-07-12 00:32:27 -0700

    Project initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Auto Scout</title>
+        <meta name='description' content='A application that help you find auto models information' />
+      </Helmet>
     </div>
   );
 }
