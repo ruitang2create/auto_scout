712e3d16dec2e582f10ec8275f1ed6461be493c8
ruitang2create <trtdytr93629@gmail.com>
2021-07-19 02:34:31 -0500

    Added pagination to the results page

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Spinner, Offcanvas, FormControl, Form, Row, Col } from 'react-bootstrap';
+import { Spinner, Offcanvas, FormControl, Form, Row, Col, Button } from 'react-bootstrap';
 import Layout from '../components/Layout';
 import ResultItem from '../components/ResultItem';
 import styles from '../styles/Results.module.css';
 import { BsCaretRightFill } from 'react-icons/bs';
+import { BiChevronsLeft, BiChevronsRight, BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 
 export default function Results(props) {
     const [modelsList, setModelsList] = useState([]);
     const [notFound, setNotFound] = useState(false);
     const [sidebarShow, setSidebarShow] = useState(false);
     const [typeUnset, setTypeUnset] = useState(true);
+    const [itemsPerPage, setItemsPerpage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currFirstItem, setCurrFirstItem] = useState(1);
+    const MAX_ITEMS = 5; // max number of pagination items that are shown at a time
 
     const closeSidebar = () => setSidebarShow(false);
     const openSidebar = () => setSidebarShow(true);
@@ -18,6 +23,7 @@ export default function Results(props) {
     const setMakeHandler = (event) => {
         const makeName = event.target.value;
         setTypeUnset(true);
+        setCurrentPage(1);
         if (makeName !== '') {
             props.searchSetter({
                 ...props.searchCriteria,
@@ -31,6 +37,7 @@ export default function Results(props) {
     const setTypeHandler = (event) => {
         const typeName = event.target.value;
         setTypeUnset(false);
+        setCurrentPage(1);
         props.searchSetter({
             ...props.searchCriteria,
             type: typeName,
@@ -38,6 +45,7 @@ export default function Results(props) {
     };
 
     const setYearHandler = (event) => {
+        setCurrentPage(1);
         const year = event.target.value;
         if (year !== '' && (year < props.yearLimit.MIN || year > props.yearLimit.MAX)) {
             event.target.value = '';
@@ -74,8 +82,9 @@ export default function Results(props) {
     }
 
     const renderResults = () => {
+        const itemsToShow = modelsList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
         return (
-            modelsList.map((model, index) => {
+            itemsToShow.map((model, index) => {
                 model = {
                     ...model,
                     year: props.searchCriteria.year,
@@ -87,9 +96,46 @@ export default function Results(props) {
         );
     }
 
+    const handlePageItemClick = (event) => {
+        setCurrentPage(event.target.value);
+    }
+
+    const renderPagination = () => {
+        const pgNum = Math.ceil(modelsList.length / itemsPerPage);
+        const pageItems = [];
+        const paginationSize = window.matchMedia("(max-width: 800px)").matches ? 'sm' : 'normal';
+        for (let i = 1; i < pgNum + 1; i++) {
+            pageItems.push(i);
+        }
+
+        return (
+            <div className={styles.paginationWrapper}>
+                <Button variant='dark' className={styles.paginationBtn} onClick={() => setCurrentPage(pgNum >= 1 ? 1 : 0)} ><BiChevronsLeft /></Button>
+                <Button variant='dark' className={styles.paginationBtn} onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)} ><BiChevronLeft /></Button>
+                <div className={styles.selectWrapper}>
+                    <FormControl as='select' onChange={handlePageItemClick} value={currentPage} style={{ textAlign: 'center' }}>
+                        {
+                            pageItems.map((pageItem, index) => {
+                                return (
+                                    <option key={index} value={pageItem}>{pageItem}</option>
+                                );
+                            })
+                        }
+                    </FormControl>
+                </div>
+                <Button variant='dark' className={styles.paginationBtn} onClick={() => setCurrentPage(currentPage < pgNum ? currentPage + 1 : currentPage)} ><BiChevronRight /></Button>
+                <Button variant='dark' className={styles.paginationBtn} onClick={() => setCurrentPage(pgNum >= 1 ? pgNum : 0)} ><BiChevronsRight /></Button>
+            </div>
+        );
+    }
+
     useEffect(() => {
         getModels(); // eslint-disable-next-line
     }, [props.searchCriteria]);
+
+    useEffect(() => {
+
+    });
 
     return (
         <Layout title='Results Page'>
@@ -97,6 +143,7 @@ export default function Results(props) {
                 {`All matches for ${props.searchCriteria.year} ${props.searchCriteria.make} ${props.searchCriteria.type}`.trim() + ':'}
             </div>
             <div className={styles.resultsContainer}>
+                {renderPagination()}
                 <div className={styles.resultsTableHead}>
                     <div>Year</div>
                     <div>Make</div>
